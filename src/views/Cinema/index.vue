df115eab7859c6e5d812a814ee8f359be0ae7909
zjy <526419266@qq.com>
2019-08-24 08:48:58 +0800

    基本文件结构配置完毕，页面结构搭建完毕

---
+++ src/views/Cinema/index.vue
@@ -0,0 +1,22 @@
+<template>
+    <div id="main" >
+        <Header title="猫猫电影院" />
+         hello Cinema
+       <TabBar></TabBar>
+    </div>
+</template>
+
+<script>
+import Header from "@/components/Header";
+import TabBar from "@/components/TabBar";
+export default {
+    name:"Cinema",
+    components: {
+        Header,
+        TabBar
+    }
+}
+</script>
+ <style >
+     
+ </style>
---
+++ src/views/Mine/index.vue
@@ -0,0 +1,21 @@
+<template>
+    <div id="main" >
+        <Header title="我的猫猫"></Header>
+       <TabBar></TabBar>
+    </div>
+</template>
+
+<script>
+import Header from "@/components/Header";
+import TabBar from "@/components/TabBar";
+export default {
+    name:"Mine",
+   components: {
+        Header,
+        TabBar
+    }
+}
+</script>
+ <style >
+     
+ </style>
